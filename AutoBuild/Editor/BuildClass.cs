2522e6d817ce94aa3b39bbe002380dbfe2dec0ba
IrosSoftware <iros0522@gmail.com>
2019-01-18 11:01:25 +0900

    Release 0.1

---
+++ AutoBuild/Editor/BuildClass.cs
@@ -0,0 +1,116 @@
+﻿using UnityEditor;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace iRosSoftware.AutoBuild
+{
+    public class BuildClass
+    {
+        public static EditorBuildSettingsScene[] GetEditorBuildSettingsScenes()
+        {
+            // ビルド対象シーンリスト
+            List<EditorBuildSettingsScene> allScene = new List<EditorBuildSettingsScene>();
+            foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+            {
+                if (scene.enabled)
+                {
+                    allScene.Add(scene);
+                }
+            }
+            return allScene.ToArray();
+        }
+
+        public static void Build()
+        {
+            string outputpath = GetProjectBuildPath();
+            BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
+            BuildOptions option = BuildOptions.None;
+            var args = Environment.GetCommandLineArgs();
+            int i, len = args.Length;
+
+
+            for (i = 0; i < len; ++i)
+            {
+                switch (args[i])
+                {
+                    case "/output":
+                        outputpath = args[i + 1];
+                        break;
+
+                    case "/platform":
+                        switch(args[i + 1])
+                        {
+                            case "Win":
+                                target = BuildTarget.StandaloneWindows;
+                                break;
+                            case "Win64":
+                                target = BuildTarget.StandaloneWindows64;
+                                break;
+                            case "OSX":
+                                target = BuildTarget.StandaloneOSX;
+                                break;
+                            case "Linux":
+                                target = BuildTarget.StandaloneLinux;
+                                break;
+                            case "Linux64":
+                                target = BuildTarget.StandaloneLinux64;
+                                break;
+                            case "LinuxU":
+                                target = BuildTarget.StandaloneLinuxUniversal;
+                                break;
+                            case "iOS":
+                                target = BuildTarget.iOS;
+                                break;
+                            case "Android":
+                                target = BuildTarget.Android;
+                                break;
+                            case "WebGL":
+                                target = BuildTarget.WebGL;
+                                break;
+                        }
+                        break;
+
+                    case "-dev":
+                        option = BuildOptions.Development;
+                        break;
+                }
+            }
+            var s = string.Format("{0}/{1}/{2}/{3}", outputpath, Application.productName,
+                DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"), Application.productName);
+
+            Debug.Log("Build Start:" + s);
+
+            Build(target, option,s);
+        }
+
+        public static void Build(BuildTarget target, BuildOptions option, string outputpath = "")
+        {
+            if (outputpath == "")
+            {
+                outputpath = GetProjectBuildPath();
+            }
+
+            var filename = Application.productName;
+#if UNITY_EDITOR
+            filename += ".exe";
+#endif
+            var builds = BuildPipeline.BuildPlayer(GetEditorBuildSettingsScenes(),
+                string.Format("{0}/{1}/{2}", outputpath,
+                DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"), filename),
+                target, option);
+        }
+
+        public static string GetProjectPath()
+        {
+            var projectFolder = System.IO.Directory.GetCurrentDirectory();
+            return projectFolder;
+        }
+
+        public static string GetProjectBuildPath()
+        {
+            return GetProjectPath() + "/Build";
+        }
+    }
+
+}
---
+++ AutoBuild/Editor/UIEditorBuildClass.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+
+namespace iRosSoftware.AutoBuild
+{
+    public class UIEditorBuildClass
+    {
+        [MenuItem("AutoBuild/Windows64")]
+        static void Windows64Build()
+        {
+            BuildClass.Build(BuildTarget.StandaloneWindows64,BuildOptions.None);
+        }
+
+        [MenuItem("AutoBuild/MacOSX")]
+        static void MacOSXBuild()
+        {
+            BuildClass.Build(BuildTarget.StandaloneOSX, BuildOptions.None);
+        }
+
+        [MenuItem("AutoBuild/Android")]
+        static void AndroidBuild()
+        {
+            BuildClass.Build(BuildTarget.Android, BuildOptions.None);
+        }
+
+        [MenuItem("AutoBuild/IOS")]
+        static void IOSBuild()
+        {
+            BuildClass.Build(BuildTarget.iOS, BuildOptions.None);
+        }
+    }
+}
